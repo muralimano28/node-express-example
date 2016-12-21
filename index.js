8e8bc161290533d5c7710fa3174cc6b9d3b559a9
Murali manohar <muralimano28@gmail.com>
2016-12-21 23:52:33 +0530

    Created mongodb and connected it using mongo client in nodejs

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 const Express = require('express');
-const App = Express();
+const BodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
 // Path constants
 const RootPath = "/";
+const AddQuotePath = "/add-quote";
 
-// Directories
+// File directories
 const IndexHTML = __dirname + "/index.html";
 
-App.listen(3000, () => {
-	console.log("Listening at 3000");
-});
+// Create a new express app.
+const App = Express();
 
-App.get(RootPath, (req, res) => {
-	res.sendFile(IndexHTML);
+// Middlewares
+App.use(BodyParser.urlencoded({ extended: true }));
+
+// Connect to database.
+MongoClient.connect("mongodb://127.0.0.1:27017/starwarsQuotesDB", (err, db) => {
+	if (err) return console.log(err);
+
+	console.log("Connected to db");
+
+	// Handlers.
+	App.listen(3000, () => {
+		console.log("Listening at 3000");
+	});
+
+	App.get(RootPath, (req, res) => {
+		db.collection("quotes").find().toArray((err, result) => {
+			if (err) return console.log("err: ", err);
+
+			console.log(result);
+		});
+
+		res.sendFile(IndexHTML);
+	});
+
+	App.post(AddQuotePath, (req, res) => {
+		db.collection("quotes").save(req.body, (err, result) => {
+			if (err) return console.log("err: ",err);
+
+			console.log(result);
+			console.log("Saved to database");
+			res.redirect(RootPath);
+		});
+	});
+
 });
